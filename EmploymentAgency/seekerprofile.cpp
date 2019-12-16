dd504610d570b0af3bafd35910fd64be71dbaac2
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2019-12-17 01:39:11 +0700

    Connect Seeker Profile with Edu & Exp

--- EmploymentAgency/seekerprofile.cpp
+++ EmploymentAgency/seekerprofile.cpp
@@ -9,9 +9,14 @@ seekerProfile::seekerProfile(QWidget *parent, int accID, int view) :
     
     // Init
     this->id = accID;
-    
-    // TODO add edu & exp view models
-    
+
+    // Init edu and exp
+    edu_model = new QSqlQueryModel();
+    ui->tableView_edu->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    exp_model = new QSqlQueryModel();
+    ui->tableView_exp->setSelectionBehavior(QAbstractItemView::SelectRows);
+
     // Loading Data
     loadProf();
     loadEdu();
@@ -20,6 +25,14 @@ seekerProfile::seekerProfile(QWidget *parent, int accID, int view) :
     // Visitor view (no button)
     if (view == 0) {
         // Close all buttons
+        ui->button_addEdu->close();
+        ui->button_addExp->close();
+        ui->button_delEdu->close();
+        ui->button_delExp->close();
+        ui->button_logOut->close();
+        ui->button_reload->close();
+        ui->button_search->close();
+        ui->button_toggleEditMode->close();
     }
     
     // Edit mode starts at read only
@@ -86,8 +99,11 @@ void seekerProfile::loadEdu()
     query.bindValue(":id", this->id);
     query.exec();
 
-     // Set query for edu model
+    // Set query for edu model
+    edu_model->setQuery(query);
 
+    // Set model
+    ui->tableView_edu->setModel(edu_model);
 }
 
 void seekerProfile::loadExp()
@@ -99,7 +115,10 @@ void seekerProfile::loadExp()
     query.exec();
 
     // Set query for exp model
+    exp_model->setQuery(query);
 
+    // Set model
+    ui->tableView_exp->setModel(exp_model);
 }
 
 int seekerProfile::getID()
@@ -162,19 +181,54 @@ void seekerProfile::on_button_toggleEditMode_clicked()
 void seekerProfile::on_button_addExp_clicked()
 {
     // Display modal for adding exp
+    seekerExperience *exp = new seekerExperience(this);
+    exp->setID(id);
+    exp->setModal(true);
+    exp->exec();
 }
 
 void seekerProfile::on_button_delExp_clicked()
 {
     // Delete the selected exp
+    if (ui->tableView_exp->selectionModel()->currentIndex().isValid()) {
+        int rowIndex = ui->tableView_exp->selectionModel()->currentIndex().row();
+
+        QSqlQuery query;
+
+        query.prepare("DELETE FROM experience_info WHERE seeker_id = :id AND company_name = :cname AND job_title = :title");
+        query.bindValue(":id", this->id);
+        query.bindValue(":cname", exp_model->index(rowIndex , 0).data().toString());
+        query.bindValue(":title", exp_model->index(rowIndex , 1).data().toString());
+
+        if (query.exec())
+            loadExp();
+    }
 }
 
 void seekerProfile::on_button_addEdu_clicked()
 {
     // Display modal for adding edu
+    seekerEducation *edu = new seekerEducation(this);
+    edu->setID(id);
+    edu->setModal(true);
+    edu->exec();
 }
 
 void seekerProfile::on_button_delEdu_clicked()
 {
     // Delete the selected edu
+    if (ui->tableView_edu->selectionModel()->currentIndex().isValid()) {
+        int rowIndex = ui->tableView_edu->selectionModel()->currentIndex().row();
+
+        QSqlQuery query;
+
+        query.prepare("DELETE FROM education_info WHERE seeker_id = :id AND school_name = :sname AND major = :major AND degree_name = :dname");
+        query.bindValue(":id", this->id);
+        query.bindValue(":sname", edu_model->index(rowIndex , 0).data().toString());
+        query.bindValue(":major", edu_model->index(rowIndex , 1).data().toString());
+        query.bindValue(":dname", edu_model->index(rowIndex , 2).data().toString());
+
+        if (query.exec())
+            loadEdu();
+    }
 }
